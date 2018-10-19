3abafec58c545893d9f603b67ae2faf9c89ef21a
CBGhaha <875675253@qq.com>
2018-10-19 16:45:07 +0800

    redux源码的一些推展

--- react/reduxs/applyMiddleware.js
+++ react/reduxs/applyMiddleware.js
@@ -15,7 +15,7 @@ export default function applyMiddleware(...middlewares) {
     //传入middlewareAPI参数并执行每一个外部函数，返回结果汇聚成数组
     chain = middlewares.map(middleware => middleware(middlewareAPI))
 
-    [next => action =>{ console.log(1);return next(action)},next => action =>{console.log(2);return next(action)}]
+    [next => action =>{ console.log(1);return next(action)},next => action =>{console.log(2);return next(action)}]//由于上一步所以这里说有函数成员都能拿到middlewareAPI
     // 数组成员1执行之后的内容 作为参数传递给 数组成员0
     next=function(action){
         console.log(2);
@@ -23,7 +23,7 @@ export default function applyMiddleware(...middlewares) {
     }
     //数组成员0执行之后的内容
     function (action){
-      console.log(1);//这里的console.log可以是一些其他针对action的操作 比如如果action是函数 或则action是一个异步操作
+      console.log(1);//这里的console.log可以是一些其他针对action的操作 比如如果action是函数 或则action是一个异步操作 在执行完必要操作后调用next或者dispatch交给下一个中间件或则reducer
       return (function(action){
           console.log(2);
           return store.dispatch(action)
