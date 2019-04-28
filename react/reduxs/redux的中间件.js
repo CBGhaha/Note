a05da503776e7665eba0afafd2e30462ba3c044a
CBGhaha <875675253@qq.com>
2019-04-28 20:05:44 +0800

    ff"

---
+++ react/reduxs/redux的中间件.js
@@ -0,0 +1,35 @@
+/**
+ * redux-thunk 允许action返回一个函数 将dispatch getState传参给函数并执行
+ */
+function thunkMiddleware({dispatch,getState}){
+    (next)=>(action)=>{
+        if(typeof next==='function'){
+            action(dispatch,getState)
+        }else{
+            next(action)
+        }
+    }
+}
+/**
+ * redux-promise 
+ * 用于处理promise类型的action 给action添加then 获取result dispatch result
+ */
+import isPromise from 'is-promise';
+import { isFSA } from 'flux-standard-action';
+
+export default function promiseMiddleware({ dispatch }) {
+  return next => action => {
+    if (!isFSA(action)) {
+      return isPromise(action) ? action.then(dispatch) : next(action);
+    }
+
+    return isPromise(action.payload)
+      ? action.payload
+          .then(result => dispatch({ ...action, payload: result }))
+          .catch(error => {
+            dispatch({ ...action, payload: error, error: true });
+            return Promise.reject(error);
+          })
+      : next(action);
+  };
+}
